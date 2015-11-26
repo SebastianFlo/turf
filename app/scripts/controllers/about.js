794eaa0f3384938db834dfd1eca3a4c19df0a953
Sebastian Florian <rebardesigndk@gmail.com>
2015-11-26 16:53:09 +0100

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name turfappApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the turfappApp
+ */
+angular.module('turfappApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
